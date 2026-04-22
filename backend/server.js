dc53afce83b5da9882e30b50f41f4fa9b182c07c
Marcelo Moll <75146662+MollMarceloMoll@users.noreply.github.com>
2026-04-22 19:19:30 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -157,6 +157,12 @@ app.use("/api", loginRouter);
 // Iniciamos el servidor
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
+//const PORT = process.env.PORT || 3000;
+//app.listen(PORT, () => {
+    //console.log(`Servidor corriendo en http://localhost:${PORT}`);
+//});
